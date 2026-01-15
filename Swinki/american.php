f6e58edf19487cf83832c7c41e16ec1319f47b78
Adrian-Buczek <buczekadrian88@gmail.com>
2026-01-15 11:20:12 +0100

    Add files via upload

--- Swinki/american.php
+++ Swinki/american.php
@@ -3,9 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hodowla Świnek Morskich</title>
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
-    
+<?php
+    $conn = mysqli_connect("localhost", "root", "", "hodowla");
+    ?>
+    <header>
+        <h1>Hodowla świnek morskich - zamów świnkowe maluszki</h1>
+    </header>
+    <main>
+        <div id="SL">
+        <div id="SG">
+            <a href="peruwianka.php">Rasa Peruwianka</a>
+            <a href="american.php">Rasa American</a>
+            <a href="crested.php">Rasa Crested</a>
+        </div>
+        <div id="SD">
+            <div id="SD"></div>
+        </div>
+        </div>
+        <aside>
+            <h3>Poznaj wszystkie rasy Świnek morskich</h3>
+            <ol>
+                <?php
+                $sql = "SELECT rasa FROM rasy";
+                $result = mysqli_query($conn, $sql);
+                while($row = mysqli_fetch_assoc($result)){
+            echo "<li> " . $row["rasa"]. "</li>";
+            }
+                ?>
+            </ol>
+        </aside>
+    </main>
+    <footer>
+        <h4>Stronę wykonał: 000000000000000</h4>
+    </footer>
+
+
 </body>
 </html>
--- Swinki/peruwianka.php
+++ Swinki/peruwianka.php
@@ -16,11 +16,77 @@
     <main>
         <div id="SL">
         <div id="SG">
-            <a href="peruwianka.html">Rasa Peruwianka</a>
-            <a href="american.html">Rasa American</a>
-            <a href="crested.html">Rasa Crested</a>
+            <a href="peruwianka.php">Rasa Peruwianka</a>
+            <a href="american.php">Rasa American</a>
+            <a href="crested.php">Rasa Crested</a>
         </div>
-        <div id="SD"></div>
+        <div id="SD">
+            <img src="peruwianka.jpg" alt="peruu">
+                 <?php
+            $sql = "SELECT * FROM rasy WHERE id = 1";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                echo "<H1>Ras: " . $row["rasa"]. "</H1>";
+                }
+            ?>
+             <?php
+            $sql = "SELECT * FROM swinki WHERE id = 6";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "
+                    <p>Data urodzenia: {$row['data_ur']} </p>
+                    <p>Oznaczenie miotu: {$row['miot']} </p>
+                ";
+                }
+            ?>
+            <h1>Świnki w tym miocie</h1>
+            <?php
+            $sql = "SELECT * FROM swinki WHERE id = 6";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "
+                <h2>{$row['imie']} - {$row['cena']}zł</h2>
+                <h4>{$row['opis']}</h4>
+                ";
+                }
+            ?>
+            <?php
+            $sql = "SELECT * FROM swinki WHERE id = 7";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "
+                <h2>{$row['imie']} - {$row['cena']}zł</h2>
+                <h4>{$row['opis']}</h4>
+                ";
+                }
+            ?>
+            <?php
+            $sql = "SELECT * FROM swinki WHERE id = 8";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "
+                <h2>{$row['imie']} - {$row['cena']}zł</h2>
+                <h4>{$row['opis']}</h4>
+                ";
+                }
+            ?>
+            <?php
+            $sql = "SELECT * FROM swinki WHERE id = 9";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "
+                <h2>{$row['imie']} - {$row['cena']}zł</h2>
+                <h4>{$row['opis']}</h4>
+                ";
+                }
+            ?>
+           
+        </div>
         </div>
         <aside>
             <h3>Poznaj wszystkie rasy Świnek morskich</h3>
@@ -28,8 +94,10 @@
                 <?php
                 $sql = "SELECT rasa FROM rasy";
                 $result = mysqli_query($conn, $sql);
+                while($row = mysqli_fetch_assoc($result)){
+            echo "<li> " . $row["rasa"]. "</li>";
+            }
                 ?>
-                
             </ol>
         </aside>
     </main>
