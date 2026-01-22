5d4b57c5a418a5afd3802529e70294f6ac3a45eb
Adrian-Buczek <buczekadrian88@gmail.com>
2026-01-22 14:49:47 +0100

    Add files via upload

--- jakiśprojekt/script.js
+++ jakiśprojekt/script.js
@@ -1,1 +1,31 @@
+const Imie = document.querySelector('#imie')
+const nazwisko = document.querySelector('#nazwisko')
+const wyslij = document.querySelector('#wyslij')
+const kom = document.querySelector('#komunikat')
 
+const liczbaa = document.querySelector('#PL')
+const liczba2 = document.querySelector('#DL')
+const oblic = document.querySelector('#oblicz')
+const wynik = document.querySelector('#wynik')
+
+
+
+
+
+
+
+function komunikat () {
+
+    kom.innerText = 'Witaj ' + Imie.value + nazwisko.value ;
+    kom.style.border = '2px dashed black';
+    kom.style.padding = '10px';
+
+}
+wyslij.addEventListener('click', komunikat)
+
+function kalkulator () {
+    wynik.innerText = 'Wynik: ' + (Number(liczbaa.value) + Number(liczba2.value));
+    wynik.style.border = '2px dashed black';
+    wynik.style.padding = '10px';
+}
+oblic.addEventListener('click', kalkulator)
