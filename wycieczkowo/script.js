26261caaf15a4f4fec45d987f6ae3139601eb60d
Adrian-Buczek <buczekadrian88@gmail.com>
2026-03-09 15:41:18 +0100

    Add files via upload

---
+++ wycieczkowo/script.js
@@ -0,0 +1,6 @@
+const przycisk = document.querySelector('#przycisk')
+const miejsce = document.querySelector('#Mwycieczki')
+const Ldoroslych = document.querySelector('#numer')
+const Ldzieci = document.querySelector('#numer2')
+const termin = document.querySelector("#data")
+
