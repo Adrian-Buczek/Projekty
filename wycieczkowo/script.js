9efe537a415c3afa679e5dcc9459bdb16840009a
Adrian-Buczek <buczekadrian88@gmail.com>
2026-03-10 09:37:09 +0100

    Add files via upload

--- wycieczkowo/script.js
+++ wycieczkowo/script.js
@@ -1,6 +1,26 @@
 const przycisk = document.querySelector('#przycisk')
-const miejsce = document.querySelector('#Mwycieczki')
-const Ldoroslych = document.querySelector('#numer')
-const Ldzieci = document.querySelector('#numer2')
-const termin = document.querySelector("#data")
+
+
+
+przycisk.addEventListener("click", (e) => {
+    e.preventDefault()
+  
+    const miejsce = document.querySelector('#Mwycieczki').value
+    const Ldoroslych = document.querySelector('#numer').value
+    const Ldzieci = document.querySelector('#numer2').value
+    const termin = document.querySelector("#data").value
+    const radio = document.querySelector("input[type='radio']:checked").value
+
+    const koszt = 1000 * (Ldoroslych / Ldzieci * 1.5)
+    console.log(koszt)
+
+    const koszta = document.querySelector("#koszta")
+    const Dwycieczki = document.querySelector("#Dwycieczki")
+    koszta.textContent = koszt
+    Dwycieczki.textContent = termin
+} )
+
+
+
+
 
