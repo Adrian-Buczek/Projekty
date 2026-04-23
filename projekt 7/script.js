0fad8dc1177cdd1c3513b13341541570ba0fd932
Adrian-Buczek <buczekadrian88@gmail.com>
2026-04-23 10:20:44 +0200

    Add files via upload

--- projekt 7/script.js
+++ projekt 7/script.js
@@ -9,10 +9,39 @@ const simie = document.querySelector('#simie')
 const snazwisko = document.querySelector('#snazwisko')
 const snrtel = document.querySelector('#snrtel')
 const semail = document.querySelector('#semail') 
+const podbt = document.querySelector('#podbt')
 
 
 function nkontakt () {
+    if(imie.value == ""){
+        simie.style.color = 'red'
+       
+        simie.textContent = 'Wpisz imie!' 
 
-    ul.innerHTML += '<li>' +imie.value+''+nazwisko.value+nrtel.value+mail.value+grupa.value+'</li>'
+    } else if(nazwisko.value == ""){
+        snazwisko.style.color = 'red'
+    
+        snazwisko.textContent = 'Wpisz nazwisko!'
+    } else if(nrtel.value == "" ){
+        snrtel.style.color = 'red'
+
+        snrtel.textContent = 'Wpisz numer telefonu!'
+
+    } else if(nrtel.value.length < 9){
+        snrtel.style.color = 'red'
+
+        snrtel.textContent = 'Wpisz prawidłowy numer telefonu!'
+    } else if(!mail.value.includes('@')){
+        semail.style.color = 'red'
+
+        semail.textContent = 'Wpisz prawidłowy adres email!'
+    } else{
+        podbt.style.color = 'lightgreen'
+        podbt.textContent = 'Kontakt dodany!'
+      ul.innerHTML += '<li>' +imie.value+nazwisko.value+'|'+nrtel.value+'|'+mail.value+'|'+grupa.value+'</li>'
+    }
+
+
+    
 }
 przycisk.addEventListener('click', nkontakt)
