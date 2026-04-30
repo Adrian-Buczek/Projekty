5d234fb9e0be045f52bbb094e356d301b0208427
Adrian-Buczek <buczekadrian88@gmail.com>
2026-04-30 10:28:31 +0200

    Add files via upload

---
+++ kwadratyprostokaty/script.js
@@ -0,0 +1,66 @@
+const bt1 = document.querySelector('#b1')
+const bt2 = document.querySelector('#b2')
+const bt3 = document.querySelector('#b3')
+const bt4 = document.querySelector('#b4')
+const bt5 = document.querySelector('#b5')
+const bt6 = document.querySelector('#b6')
+
+function zminanakoloru (){
+    bt1.style.backgroundColor = "red"
+    bt2.style.backgroundColor = "red"
+    bt3.style.backgroundColor = "red"
+    bt4.style.backgroundColor = "red"
+    bt5.style.backgroundColor = "red"
+    bt6.style.backgroundColor = "red"
+}
+bt1.addEventListener('click', zminanakoloru)
+
+function zminanakoloru2 (){
+    bt1.style.backgroundColor = "blue"
+    bt2.style.backgroundColor = "blue"
+    bt3.style.backgroundColor = "blue"
+    bt4.style.backgroundColor = "blue"
+    bt5.style.backgroundColor = "blue"
+    bt6.style.backgroundColor = "blue"
+}
+bt2.addEventListener('click', zminanakoloru2)
+
+function zminanakoloru3 (){
+    bt1.style.backgroundColor = "purple"
+    bt2.style.backgroundColor = "purple"
+    bt3.style.backgroundColor = "purple"
+    bt4.style.backgroundColor = "purple"
+    bt5.style.backgroundColor = "purple"
+    bt6.style.backgroundColor = "purple"
+}
+bt3.addEventListener('click', zminanakoloru3)
+
+function zminanakoloru4 (){
+    bt1.style.backgroundColor = "green"
+    bt2.style.backgroundColor = "green"
+    bt3.style.backgroundColor = "green"
+    bt4.style.backgroundColor = "green"
+    bt5.style.backgroundColor = "green"
+    bt6.style.backgroundColor = "green"
+}
+bt4.addEventListener('click', zminanakoloru4)
+
+function zminanakoloru5 (){
+    bt1.style.backgroundColor = "yellow"
+    bt2.style.backgroundColor = "yellow"
+    bt3.style.backgroundColor = "yellow"
+    bt4.style.backgroundColor = "yellow"
+    bt5.style.backgroundColor = "yellow"
+    bt6.style.backgroundColor = "yellow"
+}
+bt5.addEventListener('click', zminanakoloru5)
+
+function zminanakoloru6 (){
+    bt1.style.backgroundColor = "orange"
+    bt2.style.backgroundColor = "orange"
+    bt3.style.backgroundColor = "orange"
+    bt4.style.backgroundColor = "orange"
+    bt5.style.backgroundColor = "orange"
+    bt6.style.backgroundColor = "orange"
+}
+bt6.addEventListener('click', zminanakoloru6)
