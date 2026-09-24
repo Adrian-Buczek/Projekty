e61b2773c9370f47838b73038c2c89975dca5366
Adrian-Buczek <buczekadrian88@gmail.com>
2026-09-24 11:19:34 +0200

    Add files via upload

---
+++ jakiesgury/zdobywcy.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Zdobywcy gór
+    </title>
+</head>
+<body>
+    <header>
+        <h1>klub zdobywcy gór polskich</h1>
+    </header>
+    <nav>
+        <a href="">Kwerenda1</a>
+        <a href="">Kwerenda2</a>
+        <a href="">Kwerenda3</a>
+        <a href="">Kwerenda4</a>
+    </nav>
+    <main>
+        <section id="lewa">
+
+        </section>
+        <section id="prawa">
+            <h2>Dołącz do naszego zespołu!</h2>
+            <div id="tabelka">
+                <label for="nazwisko">Nazwisko:</label>
+                <input type="text" name="nazwisko">
+                <label for="nazwisko">Imie:</label>
+                <input type="text" name="imieo">
+                <label for="nazwisko">Funkcja:</label>
+                <select name="funkcja" id="funkcja">
+                    
+                </select>
+
+            </div>
+        </section>
+    </main>
+    <footer>
+        <h2>Stronę wykonał:0000000000</h2>
+    </footer>
+</body>
+</html>
